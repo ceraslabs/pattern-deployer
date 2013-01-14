2ce6607f6efcc006638f750d50c26242e7901d99
Hongbin Lu <hongbin034@gmail.com>
2013-01-14 19:32:40 +0000

    fix the name of cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-PatternDeployer::Application.config.session_store :cookie_store, :key => '_NestedQEMU_session'
+PatternDeployer::Application.config.session_store :cookie_store, :key => '_pattern_deployer_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
