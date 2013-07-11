ff076000ed173fd3deed31d20bf46de2f1e83c8e
Hongbin Lu <hongbin034@gmail.com>
2013-07-11 17:06:31 +0000

    Avoided name conflict of credential in user level
    Credential name can be duplicated given the owner of credential is
    different.

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -20,7 +20,22 @@ class Credential < ActiveRecord::Base
 
   attr_accessible :credential_id, :for_cloud, :owner, :id
 
-  validates :credential_id, :presence => true, :uniqueness => true
+  validate :credential_id_unique
   validates :for_cloud, :inclusion => { :in => Rails.configuration.supported_clouds, :message => "cloud %{value} is not supported" }
   validates_presence_of :owner
+
+
+  def credential_id_unique
+    query = "credential_id = :credential_id AND user_id = :user_id"
+    query_params = {:credential_id => self.credential_id, :user_id => owner.id}
+    if self.id
+      query += " AND id <> :id"
+      query_params[:id] = self.id
+    end
+
+    if Credential.where(query, query_params).first
+      errors.add(:credential_id, "'#{credential_id}' have already been taken")
+    end
+  end
+
 end
