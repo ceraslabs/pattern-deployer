cf621383255f2ce30d7857b673fb1f2417d23e37
Hongbin Lu <hongbin034@gmail.com>
2013-10-30 21:49:40 +0000

    Set Devise secrete key in production only

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -246,5 +246,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 
-  config.secret_key = 'PatternDeployer'
+  config.secret_key = 'PatternDeployer' if Rails.env == 'production'
 end
