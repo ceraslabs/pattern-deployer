7ed66670601bb69790053f0f29ed03b756178f7e
Hongbin Lu <hongbin034@gmail.com>
2014-03-13 01:31:51 +0000

    Compiled assets on the fly if missing.
    This commit attemptted to fix an image not found error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ PatternDeployer::Application.configure do
   config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
