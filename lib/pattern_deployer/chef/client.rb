acd2cb2d17737deb458a81374a7f96874194fc75
Hongbin Lu <hongbin034@gmail.com>
2014-03-18 17:19:27 +0000

    Fixed an minor spacing issue

--- lib/pattern_deployer/chef/client.rb
+++ lib/pattern_deployer/chef/client.rb
@@ -20,50 +20,50 @@ require 'chef/shef/ext'
 
 module PatternDeployer
   module Chef
-	class ChefClientsManager
-	  def initialize
-		::Chef::Config.from_file(Rails.configuration.chef_config_file)
-		Shef::Extensions.extend_context_object(self)
+    class ChefClientsManager
+      def initialize
+        ::Chef::Config.from_file(Rails.configuration.chef_config_file)
+        Shef::Extensions.extend_context_object(self)
 
-		@list_of_clients = load_clients_list
-	  end
+        @list_of_clients = load_clients_list
+      end
 
-	  def load_clients_list
-		::Chef::ApiClient.list.keys
-	  end
+      def load_clients_list
+        ::Chef::ApiClient.list.keys
+      end
 
-	  @@instance = new
+      @@instance = new
 
-	  def self.instance
-		return @@instance
-	  end
+      def self.instance
+        return @@instance
+      end
 
-	  def delete(client_name)
-		return if not @list_of_clients.include?(client_name)
+      def delete(client_name)
+        return if not @list_of_clients.include?(client_name)
 
-		delete_client = ::Chef::Knife::ClientDelete.new
-		delete_client.name_args = [client_name]
-		delete_client.config[:yes] = true
-		delete_client.run
-	  rescue Net::HTTPServerException => e
-		self.reload
-		#debug
-		puts "[#{Time.now}]INFO: Failed to delete chef client #{client_name}: #{e.message}"
-		puts e.backtrace[0..20].join("\n")
-	  ensure
-		@list_of_clients.delete(client_name)
-	  end
+        delete_client = ::Chef::Knife::ClientDelete.new
+        delete_client.name_args = [client_name]
+        delete_client.config[:yes] = true
+        delete_client.run
+      rescue Net::HTTPServerException => e
+        self.reload
+        #debug
+        puts "[#{Time.now}]INFO: Failed to delete chef client #{client_name}: #{e.message}"
+        puts e.backtrace[0..20].join("\n")
+      ensure
+        @list_of_clients.delete(client_name)
+      end
 
-	  #def deregister(client_name)
-	  #  @list_of_clients.delete(client_name)
-	  #end
+      #def deregister(client_name)
+      #  @list_of_clients.delete(client_name)
+      #end
 
-	  def reload
-		@list_of_clients = load_clients_list
-	  end
+      def reload
+        @list_of_clients = load_clients_list
+      end
 
-	  private_class_method :new
+      private_class_method :new
 
-	end
+    end
   end
 end
