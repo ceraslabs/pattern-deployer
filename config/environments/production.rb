7102d6ff1a976706d3f8016cf29ac6c4f04bef4c
Hongbin Lu <hongbin034@gmail.com>
2014-03-23 19:01:12 +0000

    Revert commit 997c3287e09eddd16b7faf328660abd39f2beef2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ PatternDeployer::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
-  config.logger = Logger.new(STDOUT)
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
